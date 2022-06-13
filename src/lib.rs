b4d2a3372686a52b9e2ac35b42d3dc5dcdabc30e
Louisa <luiza.medova@phystech.edu>
2022-06-13 15:19:38 +0300

    replaced unwrap in message reply

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ async fn receive(ft_program_id: ActorId, seller: ActorId, item: &Item) {
 }
 
 fn reply(supply_chain_event: SupplyChainEvent) {
-    msg::reply(supply_chain_event, 0).unwrap();
+    msg::reply(supply_chain_event, 0).expect("Error in message reply");
 }
 
 #[derive(Default)]
@@ -122,7 +122,7 @@ impl SupplyChain {
             },
             0,
         )
-        .unwrap()
+        .expect("Error in sending Mint message to NFT contract")
         .await
         .expect("Unable to decode Vec<u8>");
 
